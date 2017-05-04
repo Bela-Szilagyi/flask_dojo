ae455e9cf872eea1ff3484cae42524d69532af77
Bela-Szilagyi <szilagyib.mail@gmal.com>
2017-05-04 22:57:23 +0200

    file i/o implemented

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
-COUNTS = {
+counts = {
     "GET": 0,
     "POST": 0,
     "DELETE": 0,
@@ -12,25 +12,52 @@ COUNTS = {
 
 @app.route("/")
 def index():
+    print(counts)
     return render_template("index.html")
 
 
 @app.route("/statistics")
 def statistic():
-    return render_template("statistics.html", counts=COUNTS)
+    data_to_render = read_from_file('request_counts.txt')
+    return render_template("statistics.html", counts=data_to_render)
 
 
 @app.route("/request-counter", methods=["GET"])
 def get_counter():
-    COUNTS["GET"] += 1
+    counts["GET"] += 1
+    write_to_file(counts, 'request_counts.txt')
     return redirect("/")
 
 
 @app.route("/request-counter", methods=["POST"])
 def post_counter():
-    COUNTS["POST"] += 1
+    counts["POST"] += 1
+    write_to_file(counts, 'request_counts.txt')
     return redirect("/")
 
+
+def write_to_file(data, file_name):
+    with open(file_name, 'w') as f:
+        get_data = 'GET: ' + str(data['GET']) + '\n'
+        f.write(get_data)
+        get_data = 'POST: ' + str(data['POST']) + '\n'
+        f.write(get_data)
+        get_data = 'DELETE: ' + str(data['DELETE']) + '\n'
+        f.write(get_data)
+        get_data = 'PUT: ' + str(data['PUT']) + '\n'
+        f.write(get_data)
+        return
+
+
+def read_from_file(file_name):
+    with open(file_name, 'r') as f:
+        datas = f.readlines()
+    results = {}
+    results['GET'] = datas[0][5:-1]
+    results['POST'] = datas[1][6:-1]
+    results['DELETE'] = datas[2][8:-1]
+    results['PUT'] = datas[3][5:-1]
+    return results
 
 if __name__ == '__main__':
     app.run(debug=True)
